cc373d8f3f8535a26aa84024815a906567e8d57d
Guillaume Manen <g.manen@c2is.fr>
2013-07-23 18:42:24 +0200

    Fixing the 'my account' form

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -4,9 +4,12 @@ namespace Bigfoot\Bundle\UserBundle\Controller;
 
 use Bigfoot\Bundle\UserBundle\Form\AccountType;
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
@@ -32,6 +35,39 @@ class UserController extends ContainerAware
 
         return array(
             'user'      => $user,
+            'edit_form' => $editForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing BigfootUser entity.
+     *
+     * @Route("/account/edit", name="user_account_edit")
+     * @Method("PUT")
+     * @Template("BigfootUserBundle::account.html.twig")
+     */
+    public function updateAction(Request $request)
+    {
+        $em = $this->container->get('doctrine')->getManager();
+
+        $entity = $this->container->get('security.context')->getToken()->getUser();
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find BigfootUser entity.');
+        }
+
+        $editForm = $this->container->get('form.factory')->create(new AccountType($this->container->get('security.encoder_factory')), $entity);
+        $editForm->submit($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return new RedirectResponse($this->container->get('router')->generate('user_account'));
+        }
+
+        return array(
+            'user'      => $entity,
             'edit_form' => $editForm->createView(),
         );
     }
