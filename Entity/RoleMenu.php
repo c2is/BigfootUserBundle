5b3d20fd27068455766648f8d7c19f4d616a1327
Guillaume Manen <g.manen@c2is.fr>
2014-12-12 11:36:25 +0100

    Refactoring : PostgreSQL

--- Entity/RoleMenu.php
+++ Entity/RoleMenu.php
@@ -18,7 +18,7 @@ class RoleMenu
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
